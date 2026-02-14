8ef571170963a4601a65f447b828283571a13697
Six2mylove <eduarsix.1419@gmail.com>
2026-02-13 19:16:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("startButton").addEventListener("click", function() {
     document.body.innerHTML = `
         <div style="text-align:center; padding-top:40vh;">
-            <h1>Bienvenida a nuestro viaje ✨</h1>
+            <h1>Bienvenida a nuestro viaje</h1>
             <p>Mes 6... y seguimos escribiendo historia.</p>
         </div>
     `;
