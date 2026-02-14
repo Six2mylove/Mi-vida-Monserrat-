a98e725dcf80a32a7a678de043e62d2e1a524e5f
Six2mylove <eduarsix.1419@gmail.com>
2026-02-13 22:16:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 window.onload = function() {
     const boton = document.getElementById("startButton");
+    const contenido = document.getElementById("mainContent");
 
     boton.addEventListener("click", function() {
-        document.body.innerHTML = `
-            <div style="text-align:center; padding-top:40vh; color:white; background-color:#0d0d0d; height:100vh;">
-                <h1>Bienvenida a nuestro viaje ✨</h1>
-                <p>Mes 6... y seguimos escribiendo historia.</p>
-            </div>
+        contenido.innerHTML = `
+            <h1>Bienvenida a nuestro viaje ✨</h1>
+            <p>Mes 6... y seguimos escribiendo historia.</p>
         `;
     });
 };
-
